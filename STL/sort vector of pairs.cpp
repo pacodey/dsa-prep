53b16bf19c0bb495d0cce76b6ad111e5d5e7d061
pacodey <parth.juhi.jain@gmail.com>
2026-05-16 22:20:18 +0530

    sort vector of pairs

---
+++ STL/sort vector of pairs.cpp
@@ -0,0 +1,44 @@
+/*
+https://www.geeksforgeeks.org/problems/sort-vector-of-pairs/1
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector <pair <int, int>> v;
+    int n;
+    cout << "enter n: ";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int age, height;
+        cout << "enter age:    ";
+        cin >> age;
+        cout << "enter height: ";
+        cin >> height;
+        v.push_back({age, height});
+    }
+    
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = 0; j < v.size(); j++)
+        {
+            if (v[j].second < v[j + 1].second)
+            {
+                int temp = v[j].second;
+                v[j].second = v[j + 1].second;
+                v[j + 1].second = temp;
+                temp = v[j].first;
+                v[j].first = v[j + 1].first;
+                v[j + 1].first = temp;
+            }
+        }
+    }
+    
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i].first << " " << v[i].second << "\n";
+    }
+}
