ff5734235b821b6edeb0bd277b5329dbb6ffc1f2
pacodey <parth.juhi.jain@gmail.com>
2026-05-23 11:42:48 +0530

    Time: 5 ms (25.45%), Space: 9.7 MB (48.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,15 +3,8 @@ class Solution
 public:
     bool isAnagram(string s, string t)
     {
-        if (s.size() != t.size())
-        {
-            return false;
-        }
-        else
-        {
-            sort(s.begin(), s.end());
-            sort(t.begin(), t.end());
-            return (s == t);
-        }
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return (s == t);
     }
 };
