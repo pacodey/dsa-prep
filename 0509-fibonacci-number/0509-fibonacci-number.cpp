dc47e179e257aa8f43ee6047e26bf63527195521
pacodey <parth.juhi.jain@gmail.com>
2026-05-25 17:13:03 +0530

    Time: 7 ms (47.96%), Space: 7.8 MB (38.35%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    int fib(int n)
+    {
+        if (n == 0 or n == 1)
+        {
+            return n;
+        }
+        else
+        {
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+};
