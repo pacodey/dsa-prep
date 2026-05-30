505e33a58f1a692961e2d978f56fdc762f397227
pacodey <parth.juhi.jain@gmail.com>
2026-05-30 15:48:18 +0530

    Time: 1 ms (12.96%), Space: 9.7 MB (44.77%) - LeetHub

--- 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.cpp
+++ 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.cpp
@@ -1,8 +1,15 @@
+auto init = []()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
 class Solution
 {
 public:
     int theMaximumAchievableX(int num, int t)
     {
-        return num + 2*t;
+        return num + 2 * t;
     }
 };
