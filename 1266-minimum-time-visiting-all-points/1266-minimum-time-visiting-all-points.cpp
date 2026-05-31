d82e7cfc83e8b5aa7d05097274f044670857ad03
pacodey <parth.juhi.jain@gmail.com>
2026-05-31 23:57:26 +0530

    Time: 0 ms (100%), Space: 13.8 MB (58%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -6,9 +6,7 @@ public:
         int t = 0;
         for (int i = 0; i < points.size() - 1; i++)
         {
-            int x = abs(points[i][0] - points[i+1][0]);
-            int y = abs(points[i][1] - points[i+1][1]);
-            t += (x > y ? x : y);
+            t += (abs(points[i][0] - points[i+1][0]) > abs(points[i][1] - points[i+1][1]) ? abs(points[i][0] - points[i+1][0]) : abs(points[i][1] - points[i+1][1]));
         }
         return t;
     }
