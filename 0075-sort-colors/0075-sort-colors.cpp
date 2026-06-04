65ef85f23e92eb0b0d7ccbcb6910451df5631dea
pacodey <parth.juhi.jain@gmail.com>
2026-06-04 17:51:25 +0530

    Time: 0 ms (100%), Space: 11.6 MB (47.56%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    void sortColors(vector<int>& nums)
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i; j < nums.size(); j++)
+            {
+                if (nums[i] > nums[j])
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+    }
+};
