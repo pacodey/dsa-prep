7c9c06a9172b39ba4d94a204cf69db41c18d53e4
pacodey <parth.juhi.jain@gmail.com>
2026-06-07 15:10:35 +0530

    Time: 0 ms (100%), Space: 8.3 MB (1.62%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -3,10 +3,6 @@ class Solution
 public:
     int trailingZeroes(int n)
     {
-        if (n < 5)
-        {
-            return 0;
-        }
         int sum = 0;
         int i = 1;
         while ((int)(n/pow(5, i)) > 0)
