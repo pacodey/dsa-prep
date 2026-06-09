04bc29aedb23b6b17caed6574b4423741017ac79
pacodey <parth.juhi.jain@gmail.com>
2026-06-09 12:01:50 +0530

    Time: 4 ms (33.07%), Space: 103.7 MB (32.68%) - LeetHub

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.cpp
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.cpp
@@ -3,8 +3,8 @@ class Solution
 public:
     long long maxTotalValue(vector <int> &nums, int k)
     {
-        int max = *max_element(nums.begin(), nums.end());
-        int min = *min_element(nums.begin(), nums.end());
-        return (unsigned long long int)k*(max - min);
+        //int max = *max_element(nums.begin(), nums.end());
+        //int min = *min_element(nums.begin(), nums.end());
+        return (unsigned long long int)k*(*max_element(nums.begin(), nums.end()) - *min_element(nums.begin(), nums.end()));
     }
 };
