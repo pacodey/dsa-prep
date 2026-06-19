6ae1f90cfc7a0476ff3c463feede0da5173fddaa
pacodey <parth.juhi.jain@gmail.com>
2026-06-19 19:35:18 +0530

    Time: 0 ms (100%), Space: 10.7 MB (73.87%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    int largestAltitude(vector <int> &gain)
+    {
+        int sum = 0;
+        int max = 0;
+        for (int i = 0; i < gain.size(); i++)
+        {
+            sum += gain[i];
+            if (max < sum)
+            {
+                max = sum;
+            }
+        }
+        return max;
+    }
+};
