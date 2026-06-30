b744bce9f5cf17e0a0a12597beb7a8d03b3a8d03
pacodey <parth.juhi.jain@gmail.com>
2026-06-30 21:55:52 +0530

    Time: 0 ms (100%), Space: 35.7 MB (79.76%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int pivotIndex(vector <int> &nums)
+    {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i];
+        }
+        int lsum = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int rsum = sum - lsum - nums[i];
+            if (lsum == rsum)
+            {
+                return i;
+            }
+            lsum += nums[i];
+        }
+        return -1;
+    }
+};
