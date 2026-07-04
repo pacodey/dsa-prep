91dbc5c972c55269de6e5a03df4785daa39a0ce0
pacodey <parth.juhi.jain@gmail.com>
2026-07-04 23:07:35 +0530

    Time: 171 ms (57.87%), Space: 12.7 MB (38.65%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+    int countPrimes(int n)
+    {
+        if (n < 2)
+        {
+            return 0;
+        }
+
+        vector <bool> prime(n, true);
+        prime[0] = false;
+        prime[1] = false;
+
+        // sieve of eratosthenes method
+        for (int i = 2; i * i < n; i++)
+        {
+            if (prime[i])
+            {
+                for (int j = 2*i; j < n; j += i)
+                {
+                    prime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 2; i < n; i++)
+        {
+            if (prime[i])
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
