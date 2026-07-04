a92219e2610f7b9710d1bdd17bdce7adc8263494
pacodey <parth.juhi.jain@gmail.com>
2026-07-04 21:59:53 +0530

    Time: 0 ms (100%), Space: 13.2 MB (61.5%) - LeetHub

---
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    vector <int> applyOperations(vector <int> &nums)
+    {
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            if (nums[i] == nums[i + 1])
+            {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+        int left = 0;
+        for (int right = 0; right < nums.size(); right++)
+        {
+            if (nums[right] != 0)
+            {
+                nums[left] = nums[right];
+                left++;
+            }
+        }
+        for (int i = left; i < nums.size(); i++)
+        {
+            nums[i] = 0;
+        }
+        return nums;
+    }
+};
