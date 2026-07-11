6186b12c1276a0f067b068d9f0be8e85dd99b400
pacodey <parth.juhi.jain@gmail.com>
2026-07-11 15:49:48 +0530

    Time: 4 ms (34.57%), Space: 33.2 MB (0.93%) - LeetHub

---
+++ 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    bool isGoodArray(vector <int> &nums)
+    {
+        int hcf = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            hcf = gcd(hcf, nums[i]);
+        }
+        return (hcf == 1);
+    }
+};
