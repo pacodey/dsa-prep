942cac5a2c081887c1569d71172b6352e6297901
pacodey <parth.juhi.jain@gmail.com>
2026-07-18 10:38:45 +0530

    Time: 0 ms (100%), Space: 16.4 MB (31.74%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
@@ -8,17 +8,3 @@ public:
         return gcd(min, max);
     }
 };
-
-int gcd(int a, int b)
-{
-    int big = (a > b ? a : b);
-    int small = (a < b ? a : b);
-    if (small == 0)
-    {
-        return big;
-    }
-    else
-    {
-        return gcd(big - (big/small)*small, small);
-    }
-}
