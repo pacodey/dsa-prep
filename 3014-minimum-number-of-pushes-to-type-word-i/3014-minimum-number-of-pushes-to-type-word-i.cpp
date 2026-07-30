fd3bc14cf981959a62a7c40afdf5e07933b89ead
pacodey <parth.juhi.jain@gmail.com>
2026-07-30 19:12:19 +0530

    Time: 0 ms (100%), Space: 8.2 MB (95.83%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -4,18 +4,8 @@ public:
     int minimumPushes(string word)
     {
         int n = word.size();
-        if (n <= 8)
-        {
-            return n;
-        }
-        if (n <= 16)
-        {
-            return 8 + 2*(n - 8);
-        }
-        if (n <= 24)
-        {
-            return 24 + 3*(n - 16);
-        }
-        return 48 + 4*(n - 24);
+        int k = n / 8;
+        int rem = n % 8;
+        return 4 * k * (k + 1) + (k + 1) * rem;
     }
 };
